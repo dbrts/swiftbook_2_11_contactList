d5f5ce825946709c5c2ba0b838969f8aa4d6ef53
Gennady Dobretsov <dobrets@gmail.com>
2022-10-19 22:46:55 +0600

    segue works

--- swiftbook_2_11_contactList/ContactListViewController.swift
+++ swiftbook_2_11_contactList/ContactListViewController.swift
@@ -17,6 +17,25 @@ class ContactListViewController: UITableViewController {
         print(contactList)
     }
 
-
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        contactList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contact", for: indexPath)
+        let contact = contactList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = contact.fullName
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let infoVC = segue.destination as? InfoViewController
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        infoVC?.person = contactList[indexPath.row]
+    }
 }
 
---
+++ swiftbook_2_11_contactList/InfoViewController.swift
@@ -0,0 +1,23 @@
+//
+//  InfoViewController.swift
+//  swiftbook_2_11_contactList
+//
+//  Created by dobrets on 19.10.2022.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    @IBOutlet var phoneLabel: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    
+    var person: Person!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        phoneLabel.text = "Phone: \(person.phone)"
+        emailLabel.text = "Email: \(person.email)"
+    }
+
+}
