d38c672c54912f8b22aaaee953b52ac139b0f07c
Gennady Dobretsov <dobrets@gmail.com>
2022-10-19 22:06:58 +0600

    Initial Commit

---
+++ swiftbook_2_11_contactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftbook_2_11_contactList
+//
+//  Created by dobrets on 19.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
